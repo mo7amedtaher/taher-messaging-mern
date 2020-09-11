37922183a482d3799e41df6943d167ae464530b4
Mohammed Taher <mtaher@elm.sa>
2020-09-11 14:52:30 +0300

    Add Sidebar Header and Search

---
+++ src/components/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyB7Rx0c25GfcO5LXqn6zho06_62_DUJd-c",
+  authDomain: "taher-messaging-mern.firebaseapp.com",
+  databaseURL: "https://taher-messaging-mern.firebaseio.com",
+  projectId: "taher-messaging-mern",
+  storageBucket: "taher-messaging-mern.appspot.com",
+  messagingSenderId: "673093367227",
+  appId: "1:673093367227:web:9f95ab1972fdc005a6809e",
+};
