406f9e59f3334c7ec6d8685355d6d5a1f5318040
Mohammed Taher <mtaher@elm.sa>
2020-09-11 19:24:43 +0300

    Add Chat Component

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -1,10 +1,64 @@
 import React from "react";
-import "./Chat.css";
 
+import "./Chat.css";
+import { Avatar, IconButton } from "@material-ui/core";
+import {
+  MoreVert,
+  SearchOutlined,
+  AttachFile,
+  InsertEmoticon,
+} from "@material-ui/icons";
+import MicIcon from "@material-ui/icons/Mic";
 const Chat = () => {
   return (
     <div className="chat">
-      <h1>Chat</h1>
+      <div className="chat__header">
+        <Avatar />
+
+        <div className="chat__headerInfo">
+          <h5>Room Name</h5>
+          <p>last seen at ....</p>
+        </div>
+        <div className="chat__headerRight">
+          <IconButton>
+            <SearchOutlined />
+          </IconButton>
+          <IconButton>
+            <AttachFile />
+          </IconButton>
+          <IconButton>
+            <MoreVert />
+          </IconButton>
+        </div>
+      </div>
+      <div className="chat__body">
+        <p className="chat__message">
+          <span className="chat__name">Mohamed</span>
+          this is a message
+          <span className="chat__timestamp">{new Date().toUTCString()}</span>
+        </p>
+
+        <p className="chat__message chat__reciever">
+          <span className="chat__name">Mohamed</span>
+          this is a message
+          <span className="chat__timestamp">{new Date().toUTCString()}</span>
+        </p>
+
+        <p className="chat__message">
+          <span className="chat__name">Mohamed</span>
+          this is a message
+          <span className="chat__timestamp">{new Date().toUTCString()}</span>
+        </p>
+      </div>
+
+      <div className="chat__footer">
+        <InsertEmoticon />
+        <form>
+          <input placeholder="Type a message" type="text" />
+          <button type="submit">send a message</button>
+        </form>
+        <MicIcon />
+      </div>
     </div>
   );
 };
---
+++ src/components/SidebarChat.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import "./SidebarChat.css";
+import { Avatar } from "@material-ui/core";
+
+const SidebarChat = () => {
+  return (
+    <div className="sidebarChat">
+      <Avatar />
+      <div className="sidebarChat__info">
+        <h2>Room Name</h2>
+        <p>this is the last message</p>
+      </div>
+    </div>
+  );
+};
+
+export default SidebarChat;
